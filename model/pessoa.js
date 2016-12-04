d41815fd592859282e26ae10aa21079e7cea7748
AllefLobo <allef.lobo@gmail.com>
2016-12-03 23:27:35 -0300

    resolvendo

--- model/pessoa.js
+++ model/pessoa.js
@@ -2,20 +2,24 @@ var mongoose = require('../connection/db');
 
 var Schema = mongoose.Schema;
 
-<<<<<<< HEAD
-var pessoaSchema = new Schema({
+
+var PessoaSchema = new Schema({
 	nome: {type:String, require:true},
   email: {type:String, require:true},
 	senha: {type:String, require:true},
-=======
-var PessoaSchema = new Schema({
-	nome: String,
-  email: String,
-	senha: String,
->>>>>>> master
 	contas: [{ type: Schema.Types.ObjectId, ref: 'Conta' }],
 	transacoes: [{ type: Schema.Types.ObjectId, ref: 'Transacao' }],
 	categorias: [{ type: Schema.Types.ObjectId, ref: 'Categoria' }]
 });
+
+PessoaSchema.statics.buscarPorId = function(id, resposta){
+  this.findOne({_id:id})
+  .exec(resposta);
+};
+
+PessoaSchema.statics.buscar = function(resposta){
+  this.find()
+  .exec(resposta);
+};
 
 module.exports = mongoose.model('Pessoa', PessoaSchema);
--- routes/rotaPessoas.js
+++ routes/rotaPessoas.js
@@ -3,27 +3,79 @@ var router = express.Router();
 var Pessoa = require('../model/pessoa');
 
 
-router.get('/pessoas', function(req, res, next) {
-	Pessoa.find(function (err, pessoas) {
-  if (err) return console.error(err);
-  console.log(pessoas);
-	res.send(pessoas);
-})
-
+router.get('/pessoa', function(req, res) {
+	Pessoa.buscar(function(erro, dados){
+    if(erro)
+      res.sendStatus(500);
+    else {
+      res.json(dados);
+    }
+  });
 });
 
+router.post('/pessoa', function(req, res) {
+	var dados = req.body.pessoa;
 
-router.post('/pessoas', function(req, res, next) {
-	console.log(req.params);
-	res.json(req.params);
+  if(!dados)
+    return res.sendStatus(400);
+
+  var json = JSON.parse(dados);
+  var pessoa = new Categoria(json);
+
+  pessoa.save(function(err){
+    if(err)
+      res.sendStatus(500);
+    else {
+      res.sendStatus(200);
+    }
+  });
 });
 
-router.put('/pessoas/:id', function(req, res, next) {
-  res.send('respond with a resource');
+router.put('/pessoa', function(req, res) {
+	var dados = req.body.pessoa;
+
+  if(!dados)
+    return res.sendStatus(400);
+
+  var json = JSON.parse(dados);
+
+  pessoaEditar = new Pessoa(json);
+
+  Pessoa.buscarPorId(pessoaEditar._id, function(err, pessoa){
+     if(err)
+        return response.sendStatus(500);
+
+        pessoa.nome = pessoaEditar.nome;
+        pessoa.email = pessoaEditar.email;
+				pessoa.senha = pessoaEditar.senha;
+
+
+        pessoa.save(function(err){
+          if(err)
+            response.sendStatus(500);
+          else{
+            response.json(pessoaEditar);
+          }
+     });
+   });
 });
 
-router.delete('/pessoas/:id', function(req, res, next) {
-  res.send('respond with a resource');
+router.delete('/pessoa', function(req, res) {
+	var dados = req.body.pessoa;
+
+  if(!dados)
+    return response.sendStatus(400);
+
+  var json = JSON.parse(dados);
+
+  var pessoaExcluir = new Pessoa(json);
+
+  Pessoa.remove({_id:pessoaExcluir._id}, function(err){
+    if(err)
+      return response.sendStatus(500);
+
+    response.sendStatus(200);
+  });
 });
 
 module.exports = router;
