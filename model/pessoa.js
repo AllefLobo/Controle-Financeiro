078764336c6995983d48cf7716b5b70595002d2b
AllefLobo <allef.lobo@gmail.com>
2016-12-03 23:10:52 -0300

    restricoes nos atributos de pessoa

--- model/pessoa.js
+++ model/pessoa.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var pessoaSchema = new Schema({
-	nome: String,
-  email: String,
-	senha: String,
+	nome: {type:String, require:true},
+  email: {type:String, require:true},
+	senha: {type:String, require:true},
 	contas: [{ type: Schema.Types.ObjectId, ref: 'Conta' }],
 	transacoes: [{ type: Schema.Types.ObjectId, ref: 'Transacao' }],
 	categorias: [{ type: Schema.Types.ObjectId, ref: 'Categoria' }]
