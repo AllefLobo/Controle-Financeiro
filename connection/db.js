f903c8cea4c31ce08986bc578508591ea8d3b40a
AllefLobo <allef.lobo@gmail.com>
2016-12-12 02:14:56 -0300

    deploy mongo

--- connection/db.js
+++ connection/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://alleflobo:lobomongo123@ds023425.mlab.com:23425/financeiro', function(error){
+mongoose.connect('mongodb://admin:admin@ds023425.mlab.com:23425/financeiro', function(error){
   if (error) {
     console.log("Error! Connection not found!");
   }
